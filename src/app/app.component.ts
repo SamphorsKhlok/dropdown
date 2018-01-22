2704746edd1703fa332803cf6f775081446118d1
Samphors Khlok <khlok.samphors@gmail.com>
2018-01-21 16:14:50 -0800

    add animation
    add module animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Demo dropdown component';
+  title = 'Demo dropdown list component';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { DropdownListComponent } from './components/dropdown-list/dropdown-list.
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [MovieService],
   bootstrap: [AppComponent]
